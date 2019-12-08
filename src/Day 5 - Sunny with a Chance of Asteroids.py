b1656c5d9283da8a75d85dff33d8d5a4bdfe221a
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-12-08 04:26:28 -0500

    Day 5 completed

--- src/Day 5 - Sunny with a Chance of Asteroids.py
+++ src/Day 5 - Sunny with a Chance of Asteroids.py
@@ -5,8 +5,6 @@ Created on Mon Dec  2 21:32:44 2019
 @author: Dan J Hamilton
 """
 
-# import pandas as pd
-
 
 # %% Import the "program" data (as a string)
 def read_program(txtfile):
@@ -38,6 +36,10 @@ class IntCode:
                                         2: 4,
                                         3: 2,
                                         4: 2,
+                                        5: 3,
+                                        6: 3,
+                                        7: 4,
+                                        8: 4,
                                         99: 1}
         self.instruction_pointer = 0
         self.memory = program
@@ -46,7 +48,6 @@ class IntCode:
         self.opcode = 0
         self.parameters = []
         self.parameter_modes = []
-        self.txtfile = txtfile
         self.verb = self.memory[2]
 
 # %%
@@ -104,124 +105,133 @@ class IntCode:
     def prt0(self):
         print('IP = {}, Instruction = {}, OpCode = {}'
               .format(self.instruction_pointer, self.instruction, self.opcode))
-        # print('OpCode = {}, Instruction length = {}, Parameters = {}'
-        #       .format(self.opcode, self.instruction_length, self.parameters))
 
     def prt(self, i, pm, adr, val):
         print('\tV{}: Mode = {}, Address = {}, Value = {}'
               .format(i, pm, adr, val))
-        # print('\tV{} mode = {}, V{} value = {}'
-        #       .format(i + 1, self.parameter_modes[i],
-        #               i + 1, self.parameters[i]))
-        # print('\tIPCode: {}, OpCode: {}, vm{}: {}, v{}: {}'
-        #       .format(self.memory[self.instruction_pointer],
-        #               self.opcode, i + 1, self.parameter_modes[i],
-        #               i + 1, self.parameters[i]))
 
     def prt1(self, x1, x2, x3):
         print('\t\tp1 = {}, p2 = {}, p3 = {} '.format(x1, x2, x3))
 
-# %%
+# %% OpCode 1 - add two values
     def opcode1(self, parameters):
         mem = self.memory
-#        ip = self.instruction_pointer
         pm1, pm2 = self.parameter_modes
-#        v1, v2 = self.parameters[:2]
 
         adr1 = self.address(0)
         adr2 = self.address(1)
-#        self.prt1(adr1, adr2, adr1 + adr2)
-#        print('\t\tp1 = {}, p2 = {}'.format())
-#        adr1 = mem[ip + 1] if pm1 == 'position' else mem[parameters[0]]
-#        adr2 = mem[ip + 2] if pm2 == 'position' else mem[parameters[1]]
 
         p1 = mem[adr1]
         p2 = mem[adr2]
-        # p1 = mem[parameters[0]] if pm1 == 'position' else mem[ip + 1]
-        # p2 = mem[parameters[1]] if pm2 == 'position' else mem[ip + 2]
-
-        # self.prt0()
-        # self.prt(1, pm1, adr1, p1)
-        # self.prt(2, pm2, adr2, p2)
-        # self.prt(3, 'position', parameters[2], p1 + p2)
-#        self.prt1(p1, p2, p1 + p2)
-#        print('\t\tp1 = {}, p2 = {}'.format())
 
         self.memory[parameters[2]] = p1 + p2
         self.instruction_pointer += self.instruction_length
-#        print('Value at Memory Location 224 =', self.memory[224])
 
-# %%
+# %% OpCode 2 - multiply two values
     def opcode2(self, parameters):
-
         mem = self.memory
-#        ip = self.instruction_pointer
         pm1, pm2 = self.parameter_modes
-#        v1, v2 = self.parameters[:2]
 
         adr1 = self.address(0)
         adr2 = self.address(1)
-#        self.prt1(adr1, adr2, adr1 * adr2)
-#        print('\t\tp1 = {}, p2 = {}'.format(adr1, adr2))
-#        adr1 = mem[ip + 1] if pm1 == 'position' else mem[parameters[0]]
-#        adr2 = mem[ip + 2] if pm2 == 'position' else mem[parameters[1]]
 
         p1 = mem[adr1]
         p2 = mem[adr2]
-        # p1 = mem[parameters[0]] if pm1 == 'position' else mem[ip + 1]
-        # p2 = mem[parameters[1]] if pm2 == 'position' else mem[ip + 2]
-
-        # self.prt0()
-        # self.prt(1, pm1, adr1, p1)
-        # self.prt(2, pm2, adr2, p2)
-        # self.prt(3, 'position', parameters[2], p1 * p2)
-        # self.prt(0)
-        # self.prt(1)
-#        self.prt1(p1, p2, p1 * p2)
-#        print('\t\tp1 = {}, p2 = {}'.format(p1, p2))
 
         self.memory[parameters[2]] = p1 * p2
         self.instruction_pointer += self.instruction_length
-#        print('Value at Memory Location 224 =', self.memory[224])
 
-# %%
+# %% OpCode 3 - ask for input
     def opcode3(self, parameters):
         self.prt0()
-        # print('Value at Memory Location 224 =', self.memory[224])
 
         self.memory[parameters[0]] = \
             int(input('Which System ID are we testing? '))
         self.instruction_pointer += self.instruction_length
 
-# %%
+# %% OpCode 4 - output a result
     def opcode4(self, parameters):
 
         mem = self.memory
-        pm1 = self.parameter_modes[0]
-#        v1 = self.parameters[0]
 
         adr1 = self.address(0)
         p1 = mem[adr1]
 
-        # self.prt0()
-        # self.prt(1, pm1, adr1, p1)
-#        print('p1 = {}'.format(p1))
-
         if p1:
-            print('Day 5 - Part 1 - Diagnostic Code = {:,d}'.format(p1))
+            print('Diagnostic Code = {:,d}'.format(p1))
         self.instruction_pointer += self.instruction_length
 #        print('Value at Memory Location 224 =', self.memory[224])
 
-# %%
+# %% OpCode 5 - jump-if-true
+    def opcode5(self, parameters):
+
+        mem = self.memory
+        pm1, pm2 = self.parameter_modes
+
+        adr1 = self.address(0)
+        adr2 = self.address(1)
+
+        p1 = mem[adr1]
+        p2 = mem[adr2]
+
+        if p1:
+            self.instruction_pointer = p2
+        else:
+            self.instruction_pointer += self.instruction_length
+
+# %% OpCode 6 - jump-if-false
+    def opcode6(self, parameters):
+        mem = self.memory
+        pm1, pm2 = self.parameter_modes
+
+        adr1 = self.address(0)
+        adr2 = self.address(1)
+
+        p1 = mem[adr1]
+        p2 = mem[adr2]
+
+        if not(p1):
+            self.instruction_pointer = p2
+        else:
+            self.instruction_pointer += self.instruction_length
+
+# %% OpCode 7 - less than
+    def opcode7(self, parameters):
+        mem = self.memory
+        pm1, pm2 = self.parameter_modes
+
+        adr1 = self.address(0)
+        adr2 = self.address(1)
+
+        p1 = mem[adr1]
+        p2 = mem[adr2]
+
+        self.memory[parameters[2]] = 1 if p1 < p2 else 0
+        self.instruction_pointer += self.instruction_length
+
+# %% OpCode 8 - equals
+    def opcode8(self, parameters):
+        mem = self.memory
+        pm1, pm2 = self.parameter_modes
+
+        adr1 = self.address(0)
+        adr2 = self.address(1)
+
+        p1 = mem[adr1]
+        p2 = mem[adr2]
+
+        self.memory[parameters[2]] = 1 if p1 == p2 else 0
+        self.instruction_pointer += self.instruction_length
+
+# %% OpCode 99 - terminate program
     def opcode99(self):
         if self.memory[1] == 12 and self.memory[2] == 2:
             print('Day 2 - Part 1 - Output = {:,d}'.format(self.memory[0]))
-        elif self.memory[0] == 19690720:
+        if self.memory[0] == 19690720:
             print('Day 2 - Part 2 - 100 * noun + verb = {}'
                   .format(100 * self.memory[1] + self.memory[2]))
             print('Day 2 - Part 2 - Check = {}'.format(self.memory[0]))
-        else:
-            return
+
         self.instruction_pointer = len(self.memory)
 
 # %%
@@ -243,12 +253,6 @@ class IntCode:
         # p = self.parameters
 
         self.set_instruction()
-        # self.instruction.append(self.memory[self.instruction_pointer])
-
-        # self.prt0()
-        # print('OpCode =', self.opcode, ' Instruction length =',
-        # self.instruction_length, ' Parameters =', self.parameters)
-
 
         return self.opcode, self.parameters
 
@@ -265,6 +269,14 @@ class IntCode:
                 self.opcode3(parameters)
             elif opcode == 4:
                 self.opcode4(parameters)
+            elif opcode == 5:
+                self.opcode5(parameters)
+            elif opcode == 6:
+                self.opcode6(parameters)
+            elif opcode == 7:
+                self.opcode7(parameters)
+            elif opcode == 8:
+                self.opcode8(parameters)
             elif opcode == 99:
                 self.opcode99()
             else:
@@ -272,10 +284,6 @@ class IntCode:
                           + self.memory[self.instruction_pointer + 1]),
                       'program alarm')
                 self.instruction_pointer += 4
-
-
-def preprocess_program():
-    return
 
 
 def TEST(self_initialize=True, noun=0, verb=0):
@@ -291,30 +299,36 @@ def TEST(self_initialize=True, noun=0, verb=0):
         readable_program[1] = noun
         readable_program[2] = verb
 
-#    noun=225
-#    verb=1
+    # Create IntCode instance
     intcode = IntCode(readable_program)
-#    intcode = IntCode(txtfile, noun, verb, unit)
+
+    # Run code
     intcode.run_program()
 
 
-# %%
+# %% Development Environment
 
-# temp = IntCode(txtfile="../data/adventofcode_2019_day_5_input.txt", noun=12, verb=2)
+# temp = IntCode(txtfile="../data/adventofcode_2019_day_5_input.txt",
+# noun=12, verb=2)
 # temp.run_program()
-# temp = IntCode(txtfile="../data/adventofcode_2019_day_5_input.txt", noun=95, verb=7)
+# temp = IntCode(txtfile="../data/adventofcode_2019_day_5_input.txt",
+# noun=95, verb=7)
 # temp.run_program()
 
-txtfile = "../data/adventofcode_2019_day_5_input.txt"
-program = read_program(txtfile)
-readable_program = transform_program(program)
-temp = IntCode(readable_program)
-temp.run_program()
+# txtfile = "../data/adventofcode_2019_day_5_input.txt"
+# program = read_program(txtfile)
+# readable_program = transform_program(program)
+# temp = IntCode(readable_program)
+# temp.run_program()
 
-# temp = IntCode(txtfile="../data/adventofcode_2019_day_5_input.txt", noun=225, verb=1)
+# temp = IntCode(txtfile="../data/adventofcode_2019_day_5_input.txt",
+# noun=225, verb=1)
 # temp.read_program()
 # temp.initialize_memory()
 
-# TEST()
+
+# %% Production Environment (LOL)
+
+TEST()
 # TEST(self_initialize=False, noun=12, verb=2)
 # TEST(self_initialize=False, noun=95, verb=7)
