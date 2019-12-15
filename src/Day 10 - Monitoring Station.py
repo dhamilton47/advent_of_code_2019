8eaa5da58f80a051d2ae0cc1893abda1a1d57e1e
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-12-15 09:40:51 -0500

    Update Day 10 - Monitoring Station.py

--- src/Day 10 - Monitoring Station.py
+++ src/Day 10 - Monitoring Station.py
@@ -5,10 +5,17 @@ Created on Fri Dec  6 12:04:20 2019
 @author: Dan J Hamilton
 """
 
-# if two or more points share the same slople from a given point, then points
-# beyond the closest are blocked
+# if two or more points share the same angle (theta) from a given point,
+# then points beyond the closest on that angle are blocked
+
+
+# import pandas as pd
+import math
+
 
 # %% Import the data (as a matrix)
+
+
 def read_program(txtfile):
     f = open(txtfile, "r")
     if f.mode == 'r':
@@ -32,10 +39,6 @@ def transform_program(contents):
         table.append(row)
     return table, height, width
 
-#    memory_length = len(memory)
-#    for i in range(memory_length):
-#        memory[i] = int(memory[i])
-
 
 def validate(data):
     width = len(data[0])
@@ -50,45 +53,29 @@ def point(x, y):
     return [x, y]
 
 
-def slope(pt1, pt2):
-    dy = pt2[1] - pt1[1]
-    dx = pt2[0] - pt1[0]
-    return dy / dx if dx else float("inf")
-
-
 def distance(pt1, pt2):
     dy = pt2[1] - pt1[1]
     dx = pt2[0] - pt1[0]
-    return (dy ** 2 + dx ** 2) ** 0.5
+
+    return dx, dy, (dy ** 2 + dx ** 2) ** 0.5
 
 
-def slope_intercept(pt1, pt2):
-    a = slope(pt1, pt2)
-    b = a * pt1[0] - pt1[1]
+def locate_asteroids(table, h, w):
+    '''
+    Create a list of asteroid cartesian points from an import source
 
-    return b
+    table = transformed import source
+    h, w = height, width of the table
+    '''
 
+    lst = []
 
-def unique(lst):
-    unique_lst = []
-    for x in lst:
-        if x not in unique_lst:
-            unique_lst.append(x)
-
-    # print(unique_lst)
-    # print(len(unique_lst))
-    return len(unique_lst)
-
-
-def locate_asteroids(table, height, width):
-    asteroids = []
-
-    for j in range(height):
-        for i in range(width):
+    for j in range(h):
+        for i in range(w):
             if table[j][i] == '#':
-                asteroids.append(point(i, j))
+                lst.append(point(i, j))
 
-    return asteroids
+    return lst
 
 
 def other_asteroids(a, i):
@@ -100,27 +87,31 @@ def other_asteroids(a, i):
     return x
 
 
-def calculate_slopes_matrix(asteroids):
-    slopes_matrix = []
+def number_of_asteroids_seen_from_a_given_asteroid(remaining_asteroids, base):
+    ds = []
+    thetas = []
 
-    for i in asteroids:
-        slopes = []
-        asteroids1 = other_asteroids(asteroids, i)
-        for j in asteroids1:
-            slopes.append(slope(i, j))
+    for i in remaining_asteroids:
+        dx, dy, d = distance(i, base)
+        ds.append(d)
+        thetas.append(math.atan2(dy, dx))
+        count = len(set(sorted(thetas)))
 
-        slopes_matrix.append(slopes)
-
-    return slopes_matrix
+    return count
 
 
-def unique_slopes_counts(slopes_matrix):
-    unique_counts = []
+def how_well_can_i_see(asteroids):
+    max_seen = 0
+    where_seen = []
+    for item in asteroids:
+        remaining_asteroids = other_asteroids(asteroids, item)
+        seen = number_of_asteroids_seen_from_a_given_asteroid(
+            remaining_asteroids, item)
+        if seen > max_seen:
+            max_seen = seen
+            where_seen = item
 
-    for i in slopes_matrix:
-        unique_counts.append(unique(i))
-
-    return unique_counts
+    return max_seen, where_seen
 
 
 def best_base(txtfile):
@@ -131,90 +122,49 @@ def best_base(txtfile):
     # Locate asteroids
     asteroids = locate_asteroids(table, height, width)
 
-    # Calculate slopes vector for each asteroid
-    slopes_matrix = calculate_slopes_matrix(asteroids)
+    # Calculate maximum number of visible asteroids from the best base location
+    max_seen, where_seen = how_well_can_i_see(asteroids)
 
-    # Detemine number of unique slope values for each asteroid
-    unique_counts = unique_slopes_counts(slopes_matrix)
-
-    print('Day 10, Part 1 - Max asteroids visible = {}'
-          .format(max(unique_counts)))
+    print('Day 10, Part 1 - Max asteroids visible = {} from asteroid at {}'
+          .format(max_seen, where_seen))
 
 
 # %% Development Environment
-txtfile = "../data/adventofcode_2019_day_10a_input.txt"
-contents = read_program(txtfile)
-table, height, width = transform_program(contents)
-# height = len(transformed_contents)
-# width = len(transformed_contents[0])
+# txtfile = "../data/adventofcode_2019_day_10_input.txt"
+# contents = read_program(txtfile)
+# table, height, width = transform_program(contents)
 
-# set asteroid list
-asteroids = []
+# asteroids = locate_asteroids(table, height, width)
 
-for j in range(height):
-    for i in range(width):
-        if table[j][i] == '#':
-            asteroids.append(point(i, j))
+# Convert cartesian to polar coordinates
 
-base = [5, 8]
-asteroids1 = other_asteroids(asteroids, base)
+# r = (x ** 2 + y ** 2) ** 0.5
+# theta = tan-1 (y / x)
 
+# Use Math.atan2(x,y) to determine angle in correct quadrant
 
-slopes = []
-distances = []
-for j in asteroids1:
-    # print('i = {}, j = {}, slope = {}'
-    #       .format(asteroids[0], j, slope(asteroids[0], j)))
-    # slopes.append(slope([5, 8], j))
-    slopes.append(slope(base, j))
-    distances.append(distance(base, j))
+# ds = []
+# thetas = []
 
-
-# number_of_visible_asteroids = 0
-# slopes_matrix = []
-# distances_matrix = []
-# for i in asteroids:
-#     slopes = []
-#     distances = []
-# #    asteroids1 = build_phase_choices(asteroids, [5, 8])
-#     asteroids1 = other_asteroids(asteroids, i)
-#     for j in asteroids1:
-#         # print('i = {}, j = {}, slope = {}'
-#         #       .format(asteroids[0], j, slope(asteroids[0], j)))
-#         # slopes.append(slope([5, 8], j))
-#         slopes.append(slope(i, j))
-#         distances.append(distance(i, j))
-#     slopes_matrix.append(slopes)
-#     distances_matrix.append(distances)
-
-#     # print('i = {}, seen = {}'
-#     #       .format(i, unique(slopes)))
-
-# unique_count = []
-# for i in slopes_matrix:
-#     unique_count.append(unique(i))
-#     # if unique(slopes) > number_of_visible_asteroids:
-#     #     number_of_visible_asteroids = unique(slopes)
-#     #     # print('i = {}, j = {}, seen = {}'
-#     #     #       .format(asteroids[0], j, number_of_visible_asteroids))
-#     #     point = i
-
-# print('Maximum number of visible asteroids = {} at point {}'
-#       .format(number_of_visible_asteroids, point))
-
-# %% Production Environment
-
-# txtfile = "../data/adventofcode_2019_day_10a_input.txt"
-# answer = best_base(txtfile)
-
-# txtfile = "../data/adventofcode_2019_day_10b_input.txt"
-# answer = best_base(txtfile)
-
-# txtfile = "../data/adventofcode_2019_day_10c_input.txt"
-# answer = best_base(txtfile)
-
-# txtfile = "../data/adventofcode_2019_day_10d_input.txt"
-# answer = best_base(txtfile)
+# number_of_visible_asteroids = how_well_can_i_see(asteroids)
+# print('Maximum number of visible asteroids = {}'
+#       .format(number_of_visible_asteroids))
 
 # txtfile = "../data/adventofcode_2019_day_10_input.txt"
-# answer = best_base(txtfile)
+# answer = best_base(txtfile)    
+# %% Production Environment
+
+txtfile = "../data/adventofcode_2019_day_10a_input.txt"
+answer = best_base(txtfile)
+
+txtfile = "../data/adventofcode_2019_day_10b_input.txt"
+answer = best_base(txtfile)
+
+txtfile = "../data/adventofcode_2019_day_10c_input.txt"
+answer = best_base(txtfile)
+
+txtfile = "../data/adventofcode_2019_day_10d_input.txt"
+answer = best_base(txtfile)
+
+txtfile = "../data/adventofcode_2019_day_10_input.txt"
+answer = best_base(txtfile)
