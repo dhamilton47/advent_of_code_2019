c315e39f35756008c8f569dafaa6138c488a2efe
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-12-15 15:51:26 -0500

    Update Day 10 - Monitoring Station.py

--- src/Day 10 - Monitoring Station.py
+++ src/Day 10 - Monitoring Station.py
@@ -9,7 +9,7 @@ Created on Fri Dec  6 12:04:20 2019
 # then points beyond the closest on that angle are blocked
 
 
-# import pandas as pd
+import pandas as pd
 import math
 
 
@@ -54,8 +54,8 @@ def point(x, y):
 
 
 def distance(pt1, pt2):
-    dy = pt2[1] - pt1[1]
-    dx = pt2[0] - pt1[0]
+    dx = pt2[1] - pt1[1]
+    dy = pt2[0] - pt1[0]
 
     return dx, dy, (dy ** 2 + dx ** 2) ** 0.5
 
@@ -79,39 +79,109 @@ def locate_asteroids(table, h, w):
 
 
 def other_asteroids(a, i):
+    '''
+    Take one down. Pass it around. 99 bottles of..
+
+    Create a list of remaining asteroids after choosing one as a test base
+
+    a = complete list of asteroids
+    i = asteroid chosen as test base
+    '''
+
     x = []
     for y in range(len(a)):
         if a[y] != i:
             x.append(a[y])
-
     return x
+
+# %%
 
 
 def number_of_asteroids_seen_from_a_given_asteroid(remaining_asteroids, base):
-    ds = []
+    '''
+    Return the count of asteroids which can been seen from a test asteroid.
+
+    remaining_asteroids = complete list of asteroids after removing the test
+        asteroid
+    base = asteroid chosen as test base
+    '''
+
     thetas = []
+    rs = []
 
     for i in remaining_asteroids:
-        dx, dy, d = distance(i, base)
-        ds.append(d)
+        dx, dy, r = distance(i, base)
         thetas.append(math.atan2(dy, dx))
-        count = len(set(sorted(thetas)))
+        rs.append(r)
+
+    count = len(set(sorted(thetas)))
 
     return count
 
 
 def how_well_can_i_see(asteroids):
+    '''
+    From a set of asteroids, determine the one with the maximum view of
+    other asteroids  This asteroid will be the new base.
+
+    asteroids = complete list of asteroids
+    '''
+
     max_seen = 0
     where_seen = []
+
     for item in asteroids:
         remaining_asteroids = other_asteroids(asteroids, item)
         seen = number_of_asteroids_seen_from_a_given_asteroid(
             remaining_asteroids, item)
+
         if seen > max_seen:
             max_seen = seen
             where_seen = item
 
     return max_seen, where_seen
+
+
+# %%
+
+
+def asteroids_seen_from_new_base(remaining_asteroids, base):
+    '''
+    Return the angle (theta) and distance (r) of asteroids which can been
+    seen from the new base.
+
+    remaining_asteroids = complete list of asteroids after removing the test
+        asteroid
+    base = asteroid chosen as test base
+    '''
+
+    thetas = []
+    dxs = []
+    dys = []
+    rs = []
+
+    for i in remaining_asteroids:
+        dx, dy, r = distance(i, base)
+        dxs.append(dx)
+        dys.append(dy)
+        thetas.append(math.atan2(dy, dx))
+        rs.append(r)
+
+    dx = pd.Series(dxs, name='dx')
+    dy = pd.Series(dys, name='dy')
+    theta = pd.Series(thetas, name='theta')
+    r = pd.Series(rs, name='radius')
+
+    df = pd.DataFrame()
+    df['point'] = remaining_asteroids
+    df['dx'] = dx
+    df['dy'] = dy
+    df['theta'] = theta
+    df['radius'] = r
+
+    return df
+
+# %%
 
 
 def best_base(txtfile):
@@ -128,6 +198,31 @@ def best_base(txtfile):
     print('Day 10, Part 1 - Max asteroids visible = {} from asteroid at {}'
           .format(max_seen, where_seen))
 
+    return max_seen, where_seen
+
+# %%
+
+
+def blast_rotation(txtfile):
+    # Preprocess data
+    contents = read_program(txtfile)
+    table, height, width = transform_program(contents)
+
+    # Locate asteroids
+    asteroids = locate_asteroids(table, height, width)
+
+    # Calculate maximum number of visible asteroids from the best base location
+    max_seen, where_seen = how_well_can_i_see(asteroids)
+
+    # create vector of the seen asteroid locations from the new base
+    remaining_asteroids = other_asteroids(asteroids, where_seen)
+    lst = asteroids_seen_from_new_base(remaining_asteroids, where_seen)
+    lst = lst.loc[lst.groupby('theta')['radius'].idxmin()]
+
+    # Sort the vector to enable visual determination of Part 2 answer
+    lst = lst.sort_values('theta').reset_index()
+
+    return lst
 
 # %% Development Environment
 # txtfile = "../data/adventofcode_2019_day_10_input.txt"
@@ -135,36 +230,36 @@ def best_base(txtfile):
 # table, height, width = transform_program(contents)
 
 # asteroids = locate_asteroids(table, height, width)
+# # Convert cartesian to polar coordinates
 
-# Convert cartesian to polar coordinates
+# # r = (x ** 2 + y ** 2) ** 0.5
+# # theta = tan-1 (y / x)
 
-# r = (x ** 2 + y ** 2) ** 0.5
-# theta = tan-1 (y / x)
+# # Use Math.atan2(x,y) to determine angle in correct quadrant
 
-# Use Math.atan2(x,y) to determine angle in correct quadrant
+# # ds = []
+# # thetas = []
 
-# ds = []
-# thetas = []
+# # number_of_visible_asteroids = how_well_can_i_see(asteroids)
+# # print('Maximum number of visible asteroids = {}'
+# #       .format(number_of_visible_asteroids))
 
-# number_of_visible_asteroids = how_well_can_i_see(asteroids)
-# print('Maximum number of visible asteroids = {}'
-#       .format(number_of_visible_asteroids))
+# # txtfile = "../data/adventofcode_2019_day_10a_input.txt"
+# max_seen, where_seen = best_base(txtfile)
 
-# txtfile = "../data/adventofcode_2019_day_10_input.txt"
-# answer = best_base(txtfile)    
+# remaining_asteroids = other_asteroids(asteroids, where_seen)
+
+# lst1 = asteroids_seen_from_new_base(remaining_asteroids, where_seen)
+# # lst3 = [item - math.pi / 2 for item in lst1]
+
+# x = lst1.loc[lst1.groupby('theta')['radius'].idxmin()]
+# xindex = x.index
+# lst2 = lst1.drop(xindex[:], axis=0)
+# lst3 = x.sort_values('theta').reset_index()
+
 # %% Production Environment
 
-txtfile = "../data/adventofcode_2019_day_10a_input.txt"
-answer = best_base(txtfile)
-
-txtfile = "../data/adventofcode_2019_day_10b_input.txt"
-answer = best_base(txtfile)
-
-txtfile = "../data/adventofcode_2019_day_10c_input.txt"
-answer = best_base(txtfile)
-
-txtfile = "../data/adventofcode_2019_day_10d_input.txt"
-answer = best_base(txtfile)
 
 txtfile = "../data/adventofcode_2019_day_10_input.txt"
 answer = best_base(txtfile)
+answer2 = blast_rotation(txtfile)
