fd0ed68b9985fd128548efbf6ade6360b844ff2b
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-12-29 21:19:36 -0500

    Unstarted day stub files

---
+++ src/Day 13 - Care Package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 12:04:20 2019
+
+@author: Dan J Hamilton
+"""
+
+
+import aoc
+
+
+# %% Transform the "XXXX" from a string to a list
+def transform_program(contents):
+    memory = list(contents.split(","))
+#    memory_length = len(memory)
+#    for i in range(memory_length):
+#        memory[i] = int(memory[i])
+
+    return memory
+
+
+txtfile = "../data/AoC2019_day_13_input.txt"
+contents = aoc.read_program(txtfile)
+transformed_contents = transform_program(contents)
