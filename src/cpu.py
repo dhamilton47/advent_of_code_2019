e4dca238a79ce37b568cdd92ec4ff618f487f31b
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-02 02:10:53 -0500

    Update cpu.py

--- src/cpu.py
+++ src/cpu.py
@@ -34,66 +34,9 @@ class CPU:
         self.instruction = instruction
         self.name = 'The Little Train That Could'
 
-# # %%
-#     def read_stack(self):
-#         executable = stack.pop()
+    def instruction_execute(self, computer, instruction):
+        """ Execute the Instruction """
 
-    def instruction_execute(self, computer, program, memory, io, instruction):
-        if instruction is None:
-            return instruction
-
-        inst = instruction
-        opcode = inst['opcode']
-        par = inst['parameters']
-        ip = computer.ip
-
-        if opcode == 1:
-            memory.bank[par[2]['address']] = \
-                self.add(par[0]['value'], par[1]['value'])
-
-        elif opcode == 2:
-            memory.bank[par[2]['address']] = \
-                self.multiply(par[0]['value'], par[1]['value'])
-
-        elif opcode == 3:
-            answer = io.get_input(program)
-            memory.bank[par[0]['address']] = answer
-
-        elif opcode == 4:
-            io.return_output(program, instruction)
-
-        elif opcode == 5:
-            if par[0]['value']:
-                inst['length'] = par[1]['value'] - ip
-
-        elif opcode == 6:
-            if par[0]['value'] == 0:
-                inst['length'] = par[1]['value'] - ip
-
-        elif opcode == 7:
-            memory.bank[par[2]['address']] = \
-                1 if par[0]['value'] < par[1]['value'] else 0
-
-        elif opcode == 8:
-            memory.bank[par[2]['address']] = \
-                1 if par[0]['value'] == par[1]['value'] else 0
-
-        elif opcode == 9:
-            pass
-
-
-        elif opcode == 99:
-            # Terminate Program Execution
-            return inst
-
-        else:
-            print(str(100 * memory.bank(ip) + memory.bank(ip + 1)),
-                  'program alarm')
-            sys.exit()
-
-        return inst
-
-    def instruction_execute1(self, computer, instruction):
         if instruction is None:
             return instruction
 
@@ -104,17 +47,21 @@ class CPU:
         memory = computer.memory
         io = computer.io
         ip = computer.ips[process_active]
-        print(f"Instruction (in CPU Module): {inst}")
+        # print(f"Instruction (in CPU Module): {inst}")
 
         # print(f"instruction = {instruction}")
         if opcode == 1:
             memory.bank[process_active][par[2]['address']] = \
                 self.add(par[0]['value'], par[1]['value'])
+            # print(f"ADD({par[0]['value']}, {par[1]['value']}) = "
+            #       f"{memory.bank[process_active][par[2]['address']]}")
 
         elif opcode == 2:
-            print(F"value = {par[0]['value']}, value2 = {par[1]['value']}")
+            # print(F"value = {par[0]['value']}, value2 = {par[1]['value']}")
             memory.bank[process_active][par[2]['address']] = \
                 self.multiply(par[0]['value'], par[1]['value'])
+            # print(f"MUL({par[0]['value']}, {par[1]['value']}) = "
+            #       f"{memory.bank[process_active][par[2]['address']]}")
 
         elif opcode == 3:
             answer = io.get_input(computer)
@@ -135,10 +82,14 @@ class CPU:
         elif opcode == 7:
             memory.bank[process_active][par[2]['address']] = \
                 1 if par[0]['value'] < par[1]['value'] else 0
+            # print(f"LT({par[0]['value']}, {par[1]['value']}) = "
+            #       f"{memory.bank[process_active][par[2]['address']]}")
 
         elif opcode == 8:
             memory.bank[process_active][par[2]['address']] = \
                 1 if par[0]['value'] == par[1]['value'] else 0
+            # print(f"EQ({par[0]['value']}, {par[1]['value']}) = "
+            #       f"{memory.bank[process_active][par[2]['address']]}")
 
         elif opcode == 9:
             return inst
@@ -155,73 +106,11 @@ class CPU:
         return inst
 
     def add(self, i, j):
+        """ Add two operands """
+
         return i + j
 
     def multiply(self, i, j):
-        return i * j
+        """ Multiply two operands """
 
-
-# %% CPU Class
-
-# Need to change such that cpu interfaces with stack only.
-# Need to see how this will work with IO
-
-class CPU1:
-    def __init__(self, instruction=None):
-        self.instruction = instruction
-        self.name = 'The Little Train That Could'
-
-# # %%
-#     def read_stack(self):
-#         executable = stack.pop()
-
-    def instruction_execute1(self, computer, program, memory, io, instruction):
-        if instruction is None:
-            return instruction
-
-        inst = instruction
-        opcode = inst['opcode']
-        par = inst['parameters']
-        process_active = computer.process_active
-        memory = computer.memory
-        io = computer.io
-        ip = computer.ips[process_active]
-
-        if opcode == 1:
-            memory.register[process_active][par[2]['address']] = \
-                self.add(par[0]['value'], par[1]['value'])
-        elif opcode == 2:
-            memory.register[process_active][par[2]['address']] = \
-                self.multiply(par[0]['value'], par[1]['value'])
-        elif opcode == 3:
-            answer = io.get_input(program)
-            memory.register[process_active][par[0]['address']] = answer
-        elif opcode == 4:
-            io.return_output(program, instruction)
-        elif opcode == 5:
-            if par[0]['value']:
-                inst['length'] = par[1]['value'] - ip
-        elif opcode == 6:
-            if par[0]['value'] == 0:
-                inst['length'] = par[1]['value'] - ip
-        elif opcode == 7:
-            memory.register[process_active][par[2]['address']] = \
-                1 if par[0]['value'] < par[1]['value'] else 0
-        elif opcode == 8:
-            memory.register[process_active][par[2]['address']] = \
-                1 if par[0]['value'] == par[1]['value'] else 0
-        elif opcode == 99:
-            # Terminate Program Execution
-            return inst
-        else:
-            print(str(100 * memory.address(ip) + memory.address(ip + 1)),
-                  'program alarm')
-            sys.exit()
-
-        return inst
-
-    def add(self, i, j):
-        return i + j
-
-    def multiply(self, i, j):
         return i * j
