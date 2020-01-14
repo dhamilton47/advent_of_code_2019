a342a7863f4a314818282afba7202f77b857a940
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-13 23:23:31 -0500

    Update Day 11 - Space Police.py
    Update due to I/O clean up.

--- src/Day 11 - Space Police.py
+++ src/Day 11 - Space Police.py
@@ -17,10 +17,7 @@ import aoc
 def pointing(theta, turn):
     """ Determine the direction the robot is pointing after it turns """
 
-    if turn:
-        theta -= math.pi / 2  # turn right
-    else:
-        theta += math.pi / 2  # turn left
+    theta += (math.pi / 2) * (1 if turn else -1)
 
     return theta
 
@@ -33,25 +30,32 @@ def next_coordinate(pt1, theta, turn, move):
     # print(x, y)
 
     if next_theta == math.pi / 2:
-        # x += 0
         y += move
 
     elif next_theta == math.pi:
         x += -move
-        # y += 0
 
     elif next_theta == (3 * math.pi / 2):
-        # x += 0
         y += -move
 
     elif next_theta == 0:
         x += move
-        # y += 0
 
     else:
         raise ValueError('That angle is not possible for this scenario.')
 
     return next_theta, (x, y), x, y
+
+
+def panel_color(coordinates, position):
+    """ Return the color of the panel """
+
+    if position in coordinates.keys():
+        color = coordinates[position]['color']
+    else:
+        color = 0
+
+    return color
 
 
 def print_direction(theta):
@@ -60,21 +64,22 @@ def print_direction(theta):
     if theta == math.pi / 2:
         return 'UP'
 
-    elif theta == math.pi:
+    if theta == math.pi:
         return 'LEFT'
 
-    elif theta == (3 * math.pi / 2):
+    if theta == (3 * math.pi / 2):
         return 'DOWN'
 
-    elif theta == 0:
+    if theta == 0:
         return 'RIGHT'
 
-    else:
-        raise ValueError('That angle is not possible for this scenario.')
+    raise ValueError('That angle is not possible for this scenario.')
 
 
-def run():
+def run(robot):
     """ Run the IntCode program """
+
+    print_flag = False
 
     # Starting info for position on grid to be painted
     coordinates = {(0, 0): {'color': 0, 'x': 0, 'y': 0}}
@@ -88,50 +93,75 @@ def run():
 
     # Execute intcode program
     opcode = 0
-    x_s = []
-    y_s = []
+    x_s = [0]
+    y_s = [0]
+
+    robot.emulated_input = [0]
 
     while opcode != 99:
+    # for i in range(4000):
+
         opcode = robot.process_run()
 
-        # Coordinate from
-        # Input = color currently standing on
-        if current_position in coordinates.keys():
-            current_color = coordinates[current_position]['color']
-        else:
-            current_color = 0
+        robot.emulated_input = [panel_color(coordinates, current_position)]
 
-        robot.emulated_input = current_color
+        if print_flag:
+            print(f"\nColor of panel {current_position} prior to move is "
+                  f"{['black', 'white'][robot.emulated_input]}")
 
-        # color currently standing on painted
+        color_to_paint_prior_to_moving = robot.output_value
 
-        next_color = robot.output_value
-        coordinates[current_position]['color'] = next_color
+        # opcode = robot.process_run()
+        direction_to_turn = robot.output_value
+        coordinates[current_position]['color'] = color_to_paint_prior_to_moving
 
-        # Coordinate to
+        if print_flag:
+            print(f"Robot outputs are: "
+                  f"({color_to_paint_prior_to_moving}, "
+                  f"{direction_to_turn})")
 
-        # determine next position
-        turn = robot.output_value
-        # turn = output2
-        theta, next_position, row, column = \
-            next_coordinate(current_position, theta, turn, move)
-        # print(f"from = {current_position}, {print_direction(theta)},"
-        #       f" to = {next_position}")
+        theta, next_position, x, y = \
+            next_coordinate(current_position, theta, direction_to_turn, move)
 
-        # add next position to known coordinates
+        # If next position is not in coordinates, add next position
         if next_position not in coordinates.keys():
             coordinates[next_position] = \
-                {'color': next_color, 'x': row, 'y': column}
-            x_s.append(row)
-            y_s.append(column)
+                {'color': panel_color(coordinates, next_position),
+                 'x': x,
+                 'y': y}
+
+            x_s.append(x)
+            y_s.append(y)
+
+        # Otherwise just update the color
+        else:
+            coordinates[next_position]['color'] = \
+                panel_color(coordinates, next_position)
+
+        if print_flag:
+            print(f"  from {current_position}, {print_direction(theta)},"
+                  f" to {next_position}\n"
+                  f"  color of panel {current_position} after move is "
+                  f"{['black', 'white'][color_to_paint_prior_to_moving]}\n"
+                  f"  number of unique steps is {len(coordinates)}\n"
+                  f"We are now on panel {next_position}")
 
         current_position = next_position
 
-    return coordinates, x_s, y_s
+    args = dimensions(x_s, y_s)
+
+    display = graph(coordinates, *args)
+    # output_to_file(display)
+    steps = len(coordinates)
+
+    # del coordinates
+
+    return steps, coordinates, display
 
 
-def graph(coordinates, x_s, y_s):
-    """ Create the graph of the path results """
+def dimensions(x_s, y_s):
+    """ Find the dimensions of the graph we need to create """
+
     max_x = max(x_s)
     min_x = min(x_s)
 
@@ -140,29 +170,53 @@ def graph(coordinates, x_s, y_s):
 
     range_x = max_x - min_x + 1
     range_y = max_y - min_y + 1
+    print(f"\nRange of the x-axis = {range_x}\n"
+          f"  Min x = {min_x}\n"
+          f"  Max x = {max_x}\n\n"
+          f"Range of the y-axis = {range_y}\n"
+          f"  Min y = {min_y}\n"
+          f"  Max y = {max_y}")
+
+    return (max_x, max_y, range_x, range_y)
+
+
+def graph(coordinates, *args):
+    """ Create the graph of the path results """
+
+    max_x, max_y, range_x, range_y = args
+    print(f"range x = {range_x}, range y = {range_y}")
 
     # Blank grid in the size indicated by the part 1 output
-    layout = np.array(['.'] * (range_x * range_y)).reshape((range_x, range_y))
-
+    layout = np.array(['.'] * (range_x * range_y)).reshape((range_y, range_x))
+    print(f"The shape of layout is {layout.shape}")
     # Map the coordinates output
+
     for item in list(coordinates.items()):
+        coord = list(item)[0]
         item = list(item)[1]
-        row = item['x'] + abs(min_x)
-        column = item['y'] + abs(min_y)
+        # print(f"Coordinate: {coord}, x = {item['x']}, "
+        #       f"y = {item['y']}, color = {item['color']}")
+        x = -item['x'] + abs(max_x)
+        y = -item['y'] + abs(max_y)
         color = item['color']
         # print(row, column, color)
-        layout[row, column] = '.' if color == 0 else '#'
+        layout[y, x] = '.' if color == 0 else '#'
 
-    temp = []
-    for row in range(range_x):
-        # print([''.join(layout[:, j]) for j in range(range_y)])
-        temp.append([''.join(layout[:, j]) for j in range(range_y)])
+    for row in range(len(layout)):
+        line = ''.join(layout[row])
+        print(line)
 
-    # display = [''.join([
-    #     '\n'.join(layout[i, j]) for j in range(range_y)]
-    #     ) for i in range(range_x)]
+    return layout
 
-    return temp
+def output_to_file(display):
+    """ Output the final graph to a text file so we can actually see it """
+
+    file1 = open("../data/day11_output.txt", "w")
+    for item in display:
+        print(item)
+        file1.write((item + '\n'))
+
+    file1.close()
 
 
 def intcode(library, program):
@@ -173,15 +227,22 @@ def intcode(library, program):
     computer.program_load()
     computer.flash_memory()
     computer.halt_condition = True
-    computer.process_run()
+    # computer.process_run()
 
     return computer
 
 
-def test(library, program):
+def test():
     """ main() program """
 
-    intcode(library, program)
+    robot = \
+        intcode(aoc.PROGRAMS_AVAILABLE_DICTIONARY, 'Registration Identifier')
+
+    steps = run(robot)
+
+    del robot
+
+    return steps
 
 
 # %% Development Environment
@@ -230,18 +291,11 @@ def test(library, program):
 #     current_position = next_position
 
 robot = intcode(aoc.PROGRAMS_AVAILABLE_DICTIONARY, 'Registration Identifier')
-coordinates, x_s, y_s = run()
 
-display = graph(coordinates, x_s, y_s)
-# print(display)
+steps, coordinates, display = run(robot)
 
-file1 = open("../data/day11_output.txt", "w")
-for item in display:
-    file1.write(item[0])
-
-file1.close() #to change file access modes
 
 # %% Production Environment (LOL)
 
 # if __name__ == "__main__":
-#     test(aoc.PROGRAMS_AVAILABLE_DICTIONARY, 'Registration Identifier')
+#     steps = test()
