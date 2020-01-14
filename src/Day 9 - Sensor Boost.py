113c3f03caf32cb1938bf78e3f0a828d2a072cd1
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-13 23:21:17 -0500

    Update Day 9 - Sensor Boost.py
    Update due to I/O clean up.

--- src/Day 9 - Sensor Boost.py
+++ src/Day 9 - Sensor Boost.py
@@ -18,14 +18,23 @@ def intcode(library, program):
     computer.boot()
     computer.program_load()
     computer.flash_memory()
-    computer.process_run()
 
     return computer
 
 
 def test(library, program):
     """ main() program """
-    intcode(library, program)
+
+    machine = intcode(library, program)
+    machine.emulated_input = [1]
+    machine.process_run()
+    print(f"Day 9, Part 1 - BOOST keycode = {machine.output_value}")
+
+    machine = intcode(library, program)
+    machine.emulated_input = [2]
+    machine.process_run()
+    print(f"Day 9, Part 2 - Distress signal coordinates "
+          f"= {machine.output_value}")
 
 
 # %% Production Environment (LOL)
