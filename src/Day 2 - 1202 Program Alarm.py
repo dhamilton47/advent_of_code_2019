54cf28b3932eb9e606dc10e2006153455c8192f1
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-15 19:01:30 -0500

    Update Day 2
    slightly cleaner

--- src/Day 2 - 1202 Program Alarm.py
+++ src/Day 2 - 1202 Program Alarm.py
@@ -9,58 +9,46 @@ import aoc
 
 
 # %%
-def main(txtfile, instruction_length=4):
-    code = aoc.read_program(txtfile)
-
+def main(txtfile):
     for i in range(100):
         for j in range(100):
-            memory = initiate_code_sequence(code, i, j)
-            IntCode(memory)
+            memory = initiate_code_sequence(txtfile, i, j)
+            intcode(memory)
 
 
-# %%
-def set_memory(x):
-    memory = list(x.split(","))
-    return memory
-
-
-# %%
-def initiate_code_sequence(code, x, y):
-    memory = set_memory(code)
+def initiate_code_sequence(txtfile, x, y):
+    memory = aoc.read_intcode_program(txtfile)
     memory[1] = x
     memory[2] = y
+
     return memory
 
 
-# %%
 def opcode1(memory, var1, var2, var3):
     memory[var3] = memory[var1] + memory[var2]
+
     return 4
 
 
-# %%
 def opcode2(memory, var1, var2, var3):
     memory[var3] = memory[var1] * memory[var2]
+
     return 4
 
 
-# %%
 def opcode99(memory):
     if memory[1] == 12 and memory[2] == 2:
-        print('Day 2 - Part 1 - Output = {:,d}'.format(memory[0]))
+        print(f"Day 2 - Part 1 - Output = {memory[0]:d}")
     if memory[0] == 19690720:
-        print('Day 2 - Part 2 - 100 * noun + verb = {}'
-              .format(100 * memory[1] + memory[2]))
-        print('Day 2 - Part 2 - Check = {}'.format(memory[0]))
+        print(f"Day 2 - Part 2 - 100 * noun + verb = "
+              f"{100 * memory[1] + memory[2]:d}\n"
+              f"Day 2 - Part 2 - Check = {memory[0]:d}")
+
     return len(memory)
 
 
-# %%
-def IntCode(x):
+def intcode(x):
     index = 0
-
-    for i in range(len(x)):
-        x[i] = int(x[i])
 
     while index < len(x):
         opcode = x[index]
