4aa2cd3e44eeeaf8884660325d4412b531797b15
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-16 20:32:53 -0500

    Update Day 2 - 1202 Program Alarm.py
    Rewritten to use IntCode class modules.

--- src/Day 2 - 1202 Program Alarm.py
+++ src/Day 2 - 1202 Program Alarm.py
@@ -5,67 +5,49 @@ Created on Mon Dec  2 14:34:08 2019
 @author: Dan J Hamilton
 """
 
+import sys
+
 import aoc
+from computer import Computer
 
 
 # %%
-def main(txtfile):
+def intcode(library, program):
+    """ Create an IntCode Computer """
+
+    computer = Computer(library, program)
+    computer.boot()
+    computer.flash_memory(len(computer.program.code))
+    computer.halt_condition = True
+
+    return computer
+
+
+def main(library, program):
+    """ main() program """
+
+    machine = intcode(library, program)
+    machine.memory.bank[1] = 12
+    machine.memory.bank[2] = 2
+    machine.process_run()
+    print(f"Day 2 - Part 1 - Output = {machine.memory.bank[0]:d}")
+
     for i in range(100):
         for j in range(100):
-            memory = initiate_code_sequence(txtfile, i, j)
-            intcode(memory)
+            machine = intcode(library, program)
+            machine.memory.bank[1] = i
+            machine.memory.bank[2] = j
+            machine.process_run()
+            if machine.memory.bank[0] == 19690720:
+                noun = machine.memory.bank[1]
+                verb = machine.memory.bank[2]
+                answer = 100 * noun + verb
+                print(f"Day 2 - Part 2 - 100 * noun + verb = {answer:d}\n"
+                      f"Day 2 - Part 2 - Check = {machine.memory.bank[0]:d}")
+                sys.exit()
 
 
-def initiate_code_sequence(txtfile, x, y):
-    memory = aoc.read_intcode_program(txtfile)
-    memory[1] = x
-    memory[2] = y
+# %% Production Environment (LOL)
 
-    return memory
-
-
-def opcode1(memory, var1, var2, var3):
-    memory[var3] = memory[var1] + memory[var2]
-
-    return 4
-
-
-def opcode2(memory, var1, var2, var3):
-    memory[var3] = memory[var1] * memory[var2]
-
-    return 4
-
-
-def opcode99(memory):
-    if memory[1] == 12 and memory[2] == 2:
-        print(f"Day 2 - Part 1 - Output = {memory[0]:d}")
-    if memory[0] == 19690720:
-        print(f"Day 2 - Part 2 - 100 * noun + verb = "
-              f"{100 * memory[1] + memory[2]:d}\n"
-              f"Day 2 - Part 2 - Check = {memory[0]:d}")
-
-    return len(memory)
-
-
-def intcode(x):
-    index = 0
-
-    while index < len(x):
-        opcode = x[index]
-        var1 = x[index + 1]
-        var2 = x[index + 2]
-        var3 = x[index + 3]
-
-        if opcode == 1:
-            index += opcode1(x, var1, var2, var3)
-        elif opcode == 2:
-            index += opcode2(x, var1, var2, var3)
-        elif opcode == 99:
-            index = opcode99(x)
-        else:
-            print(str(100 * x[index] + x[index + 1]), 'program alarm')
-            index += 4
-
-
-# %%
-main("../data/AoC2019_day_2_input.txt")
+if __name__ == "__main__":
+    main(aoc.PROGRAMS_AVAILABLE_DICTIONARY, 'Gravity Assist')
