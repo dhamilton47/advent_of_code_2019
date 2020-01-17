baa601b1905bb0399d537eb55f8bc0491460e3d0
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-17 01:39:34 -0500

    Update computer.py
    Refactoring in reaction to refactors in other modules.

--- src/computer.py
+++ src/computer.py
@@ -19,60 +19,45 @@ class Computer:
     """
     class Computer(library = dictionary of information regarding programs)
         Inner Classes
-            class Program - code loaded to memory for execution
-            class Memory - refers to CPU cache/memory (RAM), not ROM
-            class Instruction - calculated by the computer.
+            class CPU - executes Instruction(s)
             class IO - get data from user (keyboard) or from registers.
                     Return data to user or place into a register.
-            class CPU - executes Instruction(s)
-            class OS - ?
+            class Memory - refers to CPU cache/memory (RAM), not ROM
+            class Program - code loaded to memory for execution
+            class Instruction - calculated by the computer.
 
         Properties
             computer_name
+
             cpu
-            halt_condition
             io
-            ip (instruction pointer)
-            library
             memory
-            program_name
-            program_loaded
-            program_to_load
+            program
+
+            halt_condition
+            ip (instruction pointer)
 
         Methods
-            boot
             flash_memory
             instruction_next
             process_run
-            program_load
     """
 
-    def __init__(self, library, program_to_load):
+    def __init__(self, library, program_name):
         self.computer_name = 'HAL'
-
-        self.cpu = None
-        self.halt_condition = False
-        self.io = None
-        self.ip = 0
-        self.library = library
-        self.memory = None
-        self.program_name = None
-        self.program_loaded = None
-        self.program_to_load = program_to_load
-
-    def boot(self):
-        """
-        Initialize sub-classes the computer utilizes.
-        """
 
         self.cpu = CPU()
         self.io = IO()
         self.memory = Memory()
+        self.program = Program(library[program_name])
+
+        self.halt_condition = False
+        self.ip = 0
 
     def flash_memory(self):
         """ Load the Program code into Memory bank for execution """
 
-        self.memory.bank = self.memory.flash(self.program_loaded)
+        self.memory.bank = self.memory.flash(self.program)
 
     def instruction_next(self):
         """
@@ -95,10 +80,11 @@ class Computer:
             instruction = self.instruction_next()
             # print(f"\nInstruction (in Computer Module): "
             #       f"{instruction}")
+
             instruction = \
                 self.cpu.instruction_execute(self, instruction)
-            self.ip += instruction['length']
-            # print(self.ip)
+            self.ip = instruction['next_ip']
+
             opcode = instruction['opcode']
 
             # print(f"Halt: {self.halt_condition}, "
@@ -109,10 +95,3 @@ class Computer:
                 break
 
         return opcode
-
-    def program_load(self):
-        """ Load the Program """
-
-        program_to_load = self.library[self.program_to_load]
-        self.program_name = program_to_load['name']
-        self.program_loaded = Program(program_to_load)
