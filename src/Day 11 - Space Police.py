e62684a5b1ed17c1e61b879540f4637b9c4c7558
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2020-01-17 01:46:08 -0500

    Update Day 11 - Space Police.py
    Effects of refactors elsewhere.

--- src/Day 11 - Space Police.py
+++ src/Day 11 - Space Police.py
@@ -223,16 +223,13 @@ def intcode(library, program):
     """ Create an IntCode Computer """
 
     computer = Computer(library, program)
-    computer.boot()
-    computer.program_load()
     computer.flash_memory()
     computer.halt_condition = True
-    # computer.process_run()
 
     return computer
 
 
-def test():
+def main():
     """ main() program """
 
     robot = \
@@ -298,4 +295,4 @@ steps, coordinates, display = run(robot)
 # %% Production Environment (LOL)
 
 # if __name__ == "__main__":
-#     steps = test()
+#     steps = main()
